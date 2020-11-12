4157f1890250629bd40cac86e10379896540dcba
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-11-12 16:01:36 +0000

    Change EmployeeDatum model

--- academy20-zingtech-backend/Models/EmployeeDatum.cs
+++ academy20-zingtech-backend/Models/EmployeeDatum.cs
@@ -3,15 +3,19 @@ namespace academy20_zingtech_backend.Models
     public class EmployeeDatum
     {
         public long Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
-        public long PhoneNumber { get; set; }
+        public string MobileNumber { get; set; }
         public string Address { get; set; }
+        public string City { get; set; }
+        public string Postcode { get; set; }
         public string NextOfKin { get; set; }
-        public float Salary { get; set; }
-        public string Position { get; set; }
+        public string NextOfKinContactNumber { get; set; }
+        public int SalaryBand { get; set; }
         public string OfficeLocation { get; set; }
-        public string Manager { get; set; }
+        public string Position { get; set; }
         public string UserType { get; set; }
+        public string Manager { get; set; }
     }
 }
